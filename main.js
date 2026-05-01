a1667660add26fdfa968fef6751f25fecbc433b4
qgatsu <qgatsuml@gmail.com>
2026-05-02 03:26:58 +0900

    スケールツールの更新③

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ import {
     playerScales,
     THEME_STORAGE_KEY,
     HELP_CONTENT_PATH,
-} from "./src/state.js?v=20260502-6";
+} from "./src/state.js?v=20260502-7";
 
 const playerListEl = document.getElementById("playersList");
 const playerForm = document.getElementById("playerForm");
